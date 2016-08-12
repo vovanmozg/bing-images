9a821ef59546e20a99558e84239893242f062ea7
Vladimir Polukhin <vovanmozg@gmail.com>
2016-08-12 05:43:58 +0300

    key

--- getimages.py
+++ getimages.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+# before run add BING_API_KEY variable to env like
+# export BING_API_KEY='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+import os
 from py_bing_search import PyBingImageSearch
 filter = 'Size:medium+Color:Monochrome'
 filter = 'Face:Face'
-bing_image = PyBingImageSearch('apikey', "woman", image_filters=filter) #image_filters is optional
+bing_image = PyBingImageSearch(os.environ['BING_API_KEY'], "woman", image_filters=filter) #image_filters is optional
 first_fifty_result= bing_image.search(limit=1000, format='json') #1-50
 
 for res in (first_fifty_result):
